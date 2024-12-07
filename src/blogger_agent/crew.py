e270caa4a3a981569fee0eae8add216b1da44577
sudeep-sp <sudeepspdeveloper@gmail.com>
2024-12-07 02:41:45 +0100

    refactor: update BloggerAgent implementation and remove unused functions

--- src/blogger_agent/crew.py
+++ src/blogger_agent/crew.py
@@ -2,31 +2,15 @@ from crewai import Agent, Crew, Process, Task, LLM
 from crewai.project import CrewBase, agent, crew, task
 from crewai_tools import SerperDevTool
 import os
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
 
 search_tool = SerperDevTool(api_key=os.getenv("SERPER_API_KEY"))
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
-# llama_llm = LLM(
-#     model="ollama/llama3.2",
-#     base_url="http://localhost:11434"
-# )
-# groq_llm = LLM(
-#     model=os.getenv("MODEL_G"),
-#     temperature=0.7,
-#     api_key=GROQ_API_KEY
-# )
 
 
 @CrewBase
 class BloggerAgent():
     """BloggerAgent crew"""
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
     agents_config = 'config/agents.yaml'
     tasks_config = 'config/tasks.yaml'
 
@@ -41,8 +25,6 @@ class BloggerAgent():
         api_key=GROQ_API_KEY
     )
 
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
     def Content_Strategist(self) -> Agent:
         return Agent(
@@ -68,9 +50,6 @@ class BloggerAgent():
             llm=self.llama_llm
         )
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
     @task
     def research_task(self) -> Task:
         return Task(
@@ -87,19 +66,15 @@ class BloggerAgent():
     def reporting_task(self) -> Task:
         return Task(
             config=self.tasks_config['seo_optimization_task'],
-            output_file='report.md'
+            output_file='new_blog.md'
         )
 
     @crew
     def crew(self) -> Crew:
         """Creates the BloggerAgent crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
-
         return Crew(
             agents=self.agents,  # Automatically created by the @agent decorator
             tasks=self.tasks,  # Automatically created by the @task decorator
             process=Process.sequential,
             verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
--- src/blogger_agent/main.py
+++ src/blogger_agent/main.py
@@ -3,14 +3,9 @@ from langtrace_python_sdk import langtrace
 import sys
 import warnings
 import os
-from blogger_agent.crew import BloggerAgent
+from crew import BloggerAgent
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
-
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
 
 langtrace.init(
     api_key=os.getenv("LANGTRACE_API_KEY"),
@@ -21,48 +16,11 @@ def run():
     """
     Run the crew.
     """
+    input_topic = input("Enter the topic you want to write blog: ")
     inputs = {
-        'topic': 'AI vs ML vs DL vs Data Science',
+        'topic': input_topic,
     }
     BloggerAgent().crew().kickoff(inputs=inputs)
 
 
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI vs ML vs DL vs Data Science"
-    }
-    try:
-        BloggerAgent().crew().train(n_iterations=int(
-            sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        BloggerAgent().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI vs ML vs DL vs Data Science"
-    }
-    try:
-        BloggerAgent().crew().test(n_iterations=int(
-            sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
+run()
